a78c064dfbaef03b13237b5e0484383560b8a8b9
ourzizz <552716481@qq.com>
2016-05-04 08:32:51 +0800

    加入javascript完成查询后定位功能

--- test.php
+++ test.php
@@ -1,6 +1,16 @@
 <?php
+//mysql_query('SET NAMES utf-8');
 /*--------------------------mysql------------------*/
 $db = new mysqli('localhost','root','123123','gwycf');
+
+mysqli_set_charset($db,"utf8");
+
+//if (!$db->set_charset("utf8")) {
+    //printf("Error loading character set utf8: %s\n", $db->error)
+//} else {
+    //printf("Current character set: %s\n", $db->character_set_name())
+//}
+
 $query = 'select name,kaohao,epost,zongfen,rank from info order by epost,ecode,rank';
 $stmt = $db->stmt_init();
 $stmt->prepare($query);
